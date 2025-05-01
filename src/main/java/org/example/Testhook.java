cd79e834b50a909c08a4b799f30e4f820fa0c2f9
NehemieTuring <ewanenehemie30@gmail.com>
2025-05-01 21:46:42 +0100

    Update Testhook.java

--- src/main/java/org/example/Testhook.java
+++ src/main/java/org/example/Testhook.java
@@ -1,5 +1,7 @@
-public class Testhook{
-    public static void main (String[] args){
-        system.out.println("vérifions si le hook fonctionne");
+package org.example;
+
+public class Testhook {
+    public static void main(String[] args) {
+        System.out.println("Hello from commit hook test!");
     }
 }
